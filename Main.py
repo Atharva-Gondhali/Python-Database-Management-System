815956a5df2d2073708dec2db23e0feaed367ae6
Atharva <atharvagondhali24@gmail.com>
2021-05-05 14:17:31 +0530

    start of new project

--- Main.py
+++ Main.py
@@ -1,3 +1,5 @@
+import mysql.connector
+
 from tkinter import *
 from tkinter import ttk
 
@@ -5,7 +7,17 @@ root = Tk()
 root.geometry( "400x600" )
 
 
+mydb = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+		    passwd = "atharva123@mysql",
+		    database = "project"
+    )
 
+# Check
+# print( mydb )
 
+# CREATE A CURSOR
+my_cursor = mydb.cursor()
 
 root.mainloop()
