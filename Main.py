2fb911f06c132aa50d489bf81da702bb2cdd762f
Atharva <atharvagondhali24@gmail.com>
2021-05-07 08:23:57 +0530

    basic table made

--- Main.py
+++ Main.py
@@ -20,6 +20,17 @@ mydb = mysql.connector.connect(
 # CREATE A CURSOR
 my_cursor = mydb.cursor()
 
-# CREATE TABLE
+# CREATE A TABLE (TO BE RUN ONCE)
+my_cursor.execute("CREATE TABLE IF NOT EXISTS students (student_id INT AUTO_INCREMENT PRIMARY KEY, \
+  first_name VARCHAR(255), \
+	last_name VARCHAR(255), \
+  email_id VARCHAR(255), \
+  age TINYINT(2), \
+  age_group VARCHAR(255), \
+  gender VARCHAR(255), \
+  course VARCHAR(255), \
+  medical_com VARCHAR(255), \
+  address  VARCHAR(255))")
+
 
 root.mainloop()
