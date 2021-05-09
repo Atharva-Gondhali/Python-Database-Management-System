8d04c84a02949ec8c305dc40c729e17b41fb121c
Atharva <atharvagondhali24@gmail.com>
2021-05-09 14:11:44 +0530

    switch tabs done

--- Main.py
+++ Main.py
@@ -4,8 +4,9 @@ from tkinter import *
 from tkinter import ttk
 
 root = Tk()
-root.geometry( "840x550" )
-
+root.geometry( "880x550" )
+root.resizable( width = False, height = False )
+style = ttk.Style()
 
 # mydb = mysql.connector.connect(
 #         host = "localhost",
@@ -33,14 +34,52 @@ root.geometry( "840x550" )
 #   address  VARCHAR(255))")
 
 
-frame_menu = ttk.Frame( root, borderwidth = 3, relief = GROOVE, width = 220, height = 510 )
-frame_dash = ttk.Frame( root, borderwidth = 3, relief = GROOVE, width = 560, height = 510 )
+# FRAMES
+frame_menu = ttk.Frame( root, borderwidth = 3, relief = GROOVE, width = 200, height = 510, padding = 15 )
+frame_dash = ttk.Frame( root, borderwidth = 3, relief = GROOVE, width = 620, height = 510 )
+frame_student = ttk.Frame( root, borderwidth = 3, relief = GROOVE, width = 620, height = 510 )
 
-frm_lst = [ frame_menu, frame_dash ]
+frm_lst = [ frame_menu, frame_dash, frame_student ]
 for i in frm_lst:
   i.grid_propagate(0)
 
-frame_menu.grid( row = 0, column = 0, padx = ( 20, 15 ), pady = 20 )
+frame_menu.grid( row = 0, column = 0, padx = ( 20, 15 ), pady = 20 )      
 frame_dash.grid( row = 0, column = 1, padx = ( 5, 20 ), pady = 20 )
+
+
+# LABELS
+lbl_logo = ttk.Label( frame_menu, text = "logo/app name" )
+lbl_dash = ttk.Label( frame_dash, text = "Dashboard" )
+lbl_student = ttk.Label( frame_student, text = "Student" )
+
+lbl_logo.grid( row = 5, column = 0, pady = (30, 0) )
+lbl_dash.grid( row = 0, column = 0, padx = 50, pady = 50 )
+lbl_student.grid( row = 0, column = 0, padx = 50, pady = 50 )
+
+
+# FUNCTIONS
+def open_menu_items( frame ):
+  for i in frm_lst:
+    if not ( i == frame or i == frame_menu ): 
+      i.grid_forget()
+      
+  frame.grid_propagate(0)
+  frame.grid( row = 0, column = 1, padx = ( 5, 20 ), pady = 20 )
+
+# BUTTONS 
+style.configure( 'menu.TButton', font = ( 'Helvetica', 10 ), width = 22 )
+
+btn_dash = ttk.Button( frame_menu, text = "Dashboard", style = 'menu.TButton', command = lambda: open_menu_items( frame_dash ) )
+btn_student = ttk.Button( frame_menu, text = "Students", style = 'menu.TButton', command = lambda: open_menu_items( frame_student ) )
+btn_unknown1 = ttk.Button( frame_menu, text = ".........", style = 'menu.TButton' )
+btn_unknown2 = ttk.Button( frame_menu, text = ".........", style = 'menu.TButton' )
+btn_unknown3 = ttk.Button( frame_menu, text = "Settings", style = 'menu.TButton' )
+
+btn_dash.grid( row = 0, column = 0, ipady = 23 )
+btn_student.grid( row = 1, column = 0, ipady = 23, pady = ( 10, 0 ) )
+btn_unknown1.grid( row = 2, column = 0, ipady = 23, pady = ( 10, 0 ) )
+btn_unknown2.grid( row = 3, column = 0, ipady = 23, pady = ( 10, 0 ) )
+btn_unknown3.grid( row = 4, column = 0, ipady = 23, pady = ( 10, 0 ) )
+
 
 root.mainloop()
