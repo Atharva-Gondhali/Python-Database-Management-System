9719361289c10b7fe59c8bd59011658d60c0604f
Atharva <atharvagondhali24@gmail.com>
2021-05-13 14:05:39 +0530

    add student to database done, with fixed issue

--- Main.py
+++ Main.py
@@ -7,31 +7,23 @@ root = Tk()
 root.geometry( "900x580" )
 root.resizable( width = False, height = False )
 
-# mydb = mysql.connector.connect(
-#         host = "localhost",
-#         user = "root",
-# 		passwd = "atharva123@mysql",
-# 		database = "project"
-#     )
-
-# Check
-# print( mydb )
+# CONNECT TO DATABASE
+mydb = mysql.connector.connect( host = "localhost", user = "root", passwd = "atharva123@mysql", database = "project" )
 
 # CREATE A CURSOR
-# my_cursor = mydb.cursor()
+my_cursor = mydb.cursor()
 
 # CREATE A TABLE (TO BE RUN ONCE)
 # my_cursor.execute("CREATE TABLE IF NOT EXISTS students (student_id INT AUTO_INCREMENT PRIMARY KEY, \
-#   first_name VARCHAR(255), \
-# 	last_name VARCHAR(255), \
-#   email_id VARCHAR(255), \
-#   age TINYINT(2), \
-#   age_group VARCHAR(255), \
-#   gender VARCHAR(255), \
-#   course VARCHAR(255), \
-#   medical_com VARCHAR(255), \
-#   address  VARCHAR(255))")
+#   first_name VARCHAR(255), last_name VARCHAR(255), father_name VARCHAR(255), \
+#   email_id VARCHAR(255), age TINYINT(2), age_group VARCHAR(255), gender VARCHAR(255), \
+#   course VARCHAR(255), medical_com VARCHAR(255), address  VARCHAR(255), phone_no BIGINT(20))")
 
+# SHOW TABLE
+# my_cursor.execute("SELECT * FROM students")
+# result = my_cursor.fetchall()
+# for i in result:
+# 	print(i)
 
 # FRAMES
 frame_menu = ttk.Frame( root, borderwidth = 3, relief = GROOVE, width = 200, height = 540, padding = 15 )
@@ -58,14 +50,14 @@ lbl_logo = ttk.Label( frame_menu, text = "logo/app name" )
 lbl_logo.grid( row = 5, column = 0, pady = (30, 0) )
 
 # Label students
-lbl_student = ttk.Label( frame_student, text = "Student" )
+lbl_student = ttk.Label( frame_student, text = "Manage Students", font = ( 'Helvetica', 15 ) )
 
-lbl_student.grid( row = 0, column = 0, padx = 10, pady = 10 )
+lbl_student.grid( row = 0, column = 0, padx = 30, pady = 15, sticky = W )
 
 # Label dashboard
-lbl_dash = ttk.Label( frame_dash, text = "Dashboard" )
+lbl_dash = ttk.Label( frame_dash, text = "Dashboard", font = ( 'Helvetica', 15 ) )
 
-lbl_dash.grid( row = 0, column = 0, padx = 50, pady = 50 )
+lbl_dash.grid( row = 0, column = 0, padx = 30, pady = 15, sticky = W )
 
 
 # FUNCTIONS
@@ -129,8 +121,8 @@ def add_std():
 
 
   # COMBO BOX
-  combo_age_group = ttk.Combobox( frame_add_std, values = [1, 2, 3], width = 27 )
-  combo_gender = ttk.Combobox( frame_add_std, values = ['M', 'F'], width = 27 )
+  combo_age_group = ttk.Combobox( frame_add_std, values = ['U-12', 'U-14', 'U-16', 'U-18', 'U-25', 'Open'], width = 27 )
+  combo_gender = ttk.Combobox( frame_add_std, values = ['Male', 'Female', 'Other'], width = 27 )
   combo_course = ttk.Combobox( frame_add_std, values = ['A', 'B', 'C'], width = 27 )
   
   lst_combobox = [ combo_age_group, combo_gender, combo_course ]
@@ -146,13 +138,32 @@ def add_std():
   def cancel():
     frame_add_std.grid_remove()
 
+
   def clear_fields():
     for i in lst_entry_box:
       i.delete( 0, END )
 
 
+  def add_std_database():
+    command = "INSERT INTO students (first_name, last_name, father_name, email_id, age, age_group, gender, \
+      course, medical_com, address, phone_no) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+
+    values = ( ent_first_name.get(), ent_last_name.get(), ent_father_name.get(), ent_email_id.get(),
+      ent_age.get(), combo_age_group.get(), combo_gender.get(), combo_course.get(), ent_medical_com.get(),
+      ent_address.get(), ent_phone_number.get() )
+
+    my_cursor.execute( command, values )
+    mydb.commit()
+    clear_fields()
+
+    my_cursor.execute("SELECT * FROM students")
+    result = my_cursor.fetchall()
+    for i in result:
+    	print(i)
+
+
   # BUTTONS
-  btn_add_std = ttk.Button( frame_add_std, text = "Add Student" )
+  btn_add_std = ttk.Button( frame_add_std, text = "Add Student", command = add_std_database )
   btn_cancel = ttk.Button( frame_add_std, text = "Cancel", command = cancel )
   btn_clr_field = ttk.Button( frame_add_std, text = "Clear fields", command = clear_fields )
 
@@ -189,10 +200,12 @@ btn_unknown3.grid( row = 4, column = 0, ipady = 23, pady = ( 10, 0 ) )
 # Buttons Dashborad
 
 # Buttons Students
-btn_add_std = ttk.Button( frame_student, text = "Admit Student", command = add_std )
-btn_edit_std = ttk.Button( frame_student, text = "Edit Student" )
+btn_add_std = ttk.Button( frame_student, text = "Admit Student", width = 20, command = add_std )
+btn_edit_std = ttk.Button( frame_student, text = "Edit Student", width = 20 )
+btn_show_std = ttk.Button( frame_student, text = "View Students", width = 20 )
 
-btn_add_std.grid( row = 1, column = 0, padx = 10, pady = 10 )
-btn_edit_std.grid( row = 2, column = 0, padx = 10, pady = 10 )
+btn_add_std.grid( row = 1, column = 0, padx = 10, pady = 10, ipady = 4 )
+btn_edit_std.grid( row = 1, column = 1, padx = 10, pady = 10, ipady = 4 )
+btn_show_std.grid( row = 2, column = 0, padx = 10, pady = 10, ipady = 4 )
 
 root.mainloop()
