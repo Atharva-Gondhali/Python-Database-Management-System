917d617449066adcd0e30c5782c8025e8a6c1d3a
Atharva <atharvagondhali24@gmail.com>
2021-09-12 15:59:18 +0530

    Some buttons added in course menu

--- Course/Course_front.py
+++ Course/Course_front.py
@@ -16,3 +16,20 @@ class Course:
         # Placing
         lbl_course.grid(row=0, column=0, padx=30, pady=15,
                          sticky=W)
+
+    # Widgets - Buttons
+        # Defining
+        btn_add_std = ttk.Button(self.frm, text="Create New Cousre",
+                                 width=20)
+        btn_edit_std = ttk.Button(self.frm, text="Edit Course",
+                                  width=20)
+        btn_view_std = ttk.Button(self.frm, text=".....",
+                                  width=20)
+
+        # Placing
+        btn_add_std.grid(row=4, column=0, padx=10, pady=15,
+                         ipady=8)
+        btn_edit_std.grid(row=5, column=0, padx=10, pady=15,
+                          ipady=8)
+        btn_view_std.grid(row=6, column=0, padx=10, pady=15,
+                          ipady=8)
