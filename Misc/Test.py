afe6bf29aa5bc2cd5c4a45d66b0e72d352f9ae96
Atharva <atharvagondhali24@gmail.com>
2021-10-14 13:07:49 +0530

    some change of plans(in upcoming commits)

--- Misc/Test.py
+++ Misc/Test.py
@@ -1,24 +1,29 @@
-from tkinter import *
-from tkinter import ttk
+# from tkinter import *
+# from tkinter import ttk
+from pickle import load
 
-root = Tk()
-root.geometry("500x300")
+# root = Tk()
+# root.geometry("500x300")
 
-isTextStar = True
+# isTextStar = True
 
-def change():
-    global isTextStar
-    if isTextStar:
-        ent.configure(show = '')
-        isTextStar = False
-    else:
-        ent.configure(show = '*')
-        isTextStar = True
+# def change():
+#     global isTextStar
+#     if isTextStar:
+#         ent.configure(show = '')
+#         isTextStar = False
+#     else:
+#         ent.configure(show = '*')
+#         isTextStar = True
 
-btn = ttk.Button(root, text = "change", command = change)
-ent = ttk.Entry(root, show = '*')
+# btn = ttk.Button(root, text = "change", command = change)
+# ent = ttk.Entry(root, show = '*')
 
-btn.grid(row = 0, column = 0)
-ent.grid(row = 0, column = 1)
+# btn.grid(row = 0, column = 0)
+# ent.grid(row = 0, column = 1)
 
-root.mainloop()
+# root.mainloop()
+
+file = open("logger.pkl", "rb")
+info = load(file)
+print(info)
