cdef8ae9aff9513b732f4103f07b37f0686d3126
Atharva <atharvagondhali24@gmail.com>
2022-01-22 22:35:16 +0530

    idk

--- Course/CourseTest.py
+++ Course/CourseTest.py
@@ -7,3 +7,14 @@ class CourseTest:
         self.frm = frm
         self.frm.grid_propagate(0)  # initializing frame and class
         self.frm.grid(row=0, column=0)
+
+        # WIDGETS
+    
+        # Widgets - Labels
+        # Defining
+        lbl_add_test = ttk.Label(self.frm, text="Add Test",
+                                 font=('Helvetica', 15))
+
+        # Placing
+        lbl_add_test.grid(row=0, column=0, padx=30, pady=15,
+                          sticky=W)
