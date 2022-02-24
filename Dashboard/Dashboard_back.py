4a8ab297acf35c1b4a7d535272b9e5191e30291b
Atharva <atharvagondhali24@gmail.com>
2022-02-24 11:44:29 +0530

    dashboard done

---
+++ Dashboard/Dashboard_back.py
@@ -0,0 +1,61 @@
+# IMPORTS
+import mysql.connector  # MySQL Connector import
+from pickle import load  # Pickle module import
+
+# Extracting database login b_info from pickle b_file
+b_file = open("back_info.pkl", "rb")
+b_info = load(b_file)
+b_file.close()
+
+# Connecting to Database
+mydb = mysql.connector.connect(
+    host=b_info[0],
+    user=b_info[1],
+    passwd=b_info[2], )
+# Initializing database cursor
+my_cursor = mydb.cursor()
+
+# FUNCTIONS
+# Internal Functions
+def get_database():  # To get user specific database
+    file = open("back_info.pkl", "rb")
+    info = load(file)
+    file.close()
+
+    return info[3]
+
+def get_number_std():
+    mydb.connect(database=get_database())
+    my_cursor.execute("SELECT COUNT(*) FROM students")
+
+    return my_cursor.fetchall()
+
+def get_number_crs():
+    mydb.connect(database=get_database())
+    my_cursor.execute("SELECT COUNT(*) FROM course")
+
+    return my_cursor.fetchall()
+
+def get_best():
+    mydb.connect(database=get_database())
+    my_cursor.execute("Select first_name, last_name, spt\
+        , yt, wt, stt from students, data where \
+        student_id = std_id")
+    raw_lst = my_cursor.fetchall()
+
+    def sort(rng):
+        lst_total = []
+        for i in raw_lst:
+            lst_total.append(int(i[2])+int(i[3])+int(i[4])+int(i[5]))
+        lst_final = []
+        index = 0
+        for i in range(rng):
+            index = lst_total.index(max(lst_total))
+            lst_final.append(str(raw_lst[index][0])+" "+str(raw_lst[index][1]))
+            lst_total.pop(index)
+        return lst_final
+
+    try:
+        return sort(3)
+    except ValueError:
+        pass 
--- Dashboard/Dashboard_front.py
+++ Dashboard/Dashboard_front.py
@@ -1,5 +1,8 @@
 from tkinter import *
 from tkinter import ttk
+from Dashboard.Dashboard_back import get_number_std
+from Dashboard.Dashboard_back import get_number_crs
+from Dashboard.Dashboard_back import get_best
 
 
 class Dashboard:
@@ -12,3 +15,66 @@ class Dashboard:
 
         lbl_dash.grid(row=0, column=0, padx=30, pady=15,
                       sticky=W)
+
+        # Frame 
+        frm1 = ttk.Frame(self.frm, width=300, height=220, padding=50)
+        frm2 = ttk.Frame(self.frm, width=610, height=220, padding=(50, 30))
+        frm3 = ttk.Frame(self.frm, width=300, height=220, padding=50)
+        
+        frm1.grid(row = 1, column = 0, padx = (10, 5), pady = (10, 5))
+        frm2.grid(row = 2, column = 0, padx = (10, 5), pady = (10, 5),
+                 columnspan = 2)
+        frm3.grid(row = 1, column = 1, padx = (10, 5), pady = (10, 5))
+
+        frm1.grid_propagate(0)
+        frm2.grid_propagate(0)
+        frm3.grid_propagate(0)
+
+        #Frame1
+        lbl_std = ttk.Label(frm1, text="Students Admitted",
+                             font=('Helvetica', 14))
+        lbl_std_no = ttk.Label(frm1, text = get_number_std(),  font=('Helvetica', 14))
+
+        lbl_std.grid(row = 0, column = 0, padx = 20, pady = 10)
+        lbl_std_no.grid(row = 2, column = 0, padx = 20, pady = 10)
+
+        #Frame2
+        lbl_crs = ttk.Label(frm3, text="Courses Online",
+                             font=('Helvetica', 14))
+        lbl_crs_no = ttk.Label(frm3, text = get_number_crs(),  font=('Helvetica', 14))
+
+        lbl_crs.grid(row = 0, column = 0, padx = 20, pady = 10)
+        lbl_crs_no.grid(row = 2, column = 0, padx = 20, pady = 10)
+
+        std = get_best()
+        if std == None:
+            std = ['', '', '']
+
+        #Frame3
+        lbl_best = ttk.Label(frm2, text="Best Performers",
+                             font=('Helvetica', 14))
+        lbl_p1 = ttk.Label(frm2, text="1st",
+                             font=('Helvetica', 12))
+        lbl_p2 = ttk.Label(frm2, text="2nd",
+                             font=('Helvetica', 12))
+        lbl_p3 = ttk.Label(frm2, text="3rd",
+                             font=('Helvetica', 12))
+        lbl_1 = ttk.Label(frm2, text=std[0],
+                             font=('Helvetica', 12))
+        lbl_2 = ttk.Label(frm2, text=std[1],
+                             font=('Helvetica', 12))
+        lbl_3 = ttk.Label(frm2, text=std[2],
+                             font=('Helvetica', 12))
+        
+        lbl_best.grid(row = 0, column = 0, padx = 20, pady = 10)
+        lbl_p1.grid(row = 1, column = 0, padx = 25, pady = 10, 
+                    sticky = NSEW)
+        lbl_p2.grid(row = 1, column = 1, padx = 25, pady = 10, 
+                    sticky = NSEW)
+        lbl_p3.grid(row = 1, column = 2, padx = 25, pady = 10, 
+                    sticky = NSEW)
+        lbl_1.grid(row = 2, column = 0, padx = 25, pady = 10)
+        lbl_2.grid(row = 2, column = 1, padx = 25, pady = 10)
+        lbl_3.grid(row = 2, column = 2, padx = 25, pady = 10)
+
+        
